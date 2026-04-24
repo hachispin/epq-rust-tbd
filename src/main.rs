b437833fbc2724561705baed780475cf4eb512b2
hachispin <ganalonrainier255@gmail.com>
2026-04-24 19:09:47 +0100

    add kiss3d and egui and create a basic scene

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use kiss3d::prelude::*;
+
+// This is an example program taken from https://github.com/dimforge/kiss3d.
+// It should create a scene that has a "red, rotating cube with a light attached to the camera".
+
+#[kiss3d::main]
+async fn main() {
+    let mut window = Window::new("Kiss3d: cube").await;
+    let mut camera = OrbitCamera3d::default();
+    let mut scene = SceneNode3d::empty();
+    scene
+        .add_light(Light::point(100.0))
+        .set_position(Vec3::new(0.0, 2.0, -2.0));
+
+    let mut c = scene.add_cube(1.0, 1.0, 1.0).set_color(RED);
+
+    let rot = Quat::from_axis_angle(Vec3::Y, 0.014);
+
+    while window.render_3d(&mut scene, &mut camera).await {
+        c.rotate(rot);
+    }
 }
