2a47d9fd6df4bae8fea9ae54c29d9439e669bf7a
hachispin <ganalonrainier255@gmail.com>
2026-05-12 16:28:50 +0100

    attempt a basic grid

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,57 @@
 use kiss3d::prelude::*;
 
-// This is an example program taken from https://github.com/dimforge/kiss3d.
-// It should create a scene that has a "red, rotating cube with a light attached to the camera".
+// Ideally, these variables should by "dynamic".
+// This is something to take note of in the future.
+//
+// SPACING should be greater than LINE_WIDTH.
+
+/// Grid is square and has edges:
+/// (r, r), (-r, r), (r, -r), (-r, -r)
+const RADIUS: f32 = 250.0;
+
+/// Spacing between grid lines.
+const SPACING: f32 = 25.0;
+
+/// Width of grid lines in pixels.
+const LINE_WIDTH: f32 = 1.0;
 
 #[kiss3d::main]
 async fn main() {
-    let mut window = Window::new("Kiss3d: cube").await;
+    let mut window = Window::new("2D Grid").await;
     let mut camera = OrbitCamera3d::default();
     let mut scene = SceneNode3d::empty();
-    scene
-        .add_light(Light::point(100.0))
-        .set_position(Vec3::new(0.0, 2.0, -2.0));
 
-    let mut c = scene.add_cube(1.0, 1.0, 1.0).set_color(RED);
+    camera.set_dist(RADIUS * 3.0);
 
-    let rot = Quat::from_axis_angle(Vec3::Y, 0.014);
-
+    // Draws a 2D grid.
+    //
+    // In the future, this should be a 3D grid. A toggle for a
+    // "2D mode" would lock the camera and hide z grid lines.
     while window.render_3d(&mut scene, &mut camera).await {
-        c.rotate(rot);
+        eprintln!("{}", camera.dist());
+        let mut i = -RADIUS;
+
+        // Draws the grid lines.
+        while i <= RADIUS {
+            // Horizontal.
+            window.draw_line(
+                Vec3::new(-RADIUS, i, 0.0),
+                Vec3::new(RADIUS, i, 0.0),
+                WHITE,
+                LINE_WIDTH,
+                false,
+            );
+
+            // Vertical.
+            window.draw_line(
+                Vec3::new(i, -RADIUS, 0.0),
+                Vec3::new(i, RADIUS, 0.0),
+                WHITE,
+                LINE_WIDTH,
+                false,
+            );
+
+            i += SPACING;
+        }
     }
 }
