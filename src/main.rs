2d972a2d0639babda0c9e43446dc4cbd77cc2b21
hachispin <ganalonrainier255@gmail.com>
2026-05-16 19:55:41 +0100

    Experiment with `three-d` library
    Main motivation for (possibly) switching away from the `kiss3d` library
    is the more fine-grained control over the camera/viewport. One pain
    point with `kiss3d` was the lack of customization for "zooming".
    
    This is mitigated in `three-d` because it allows you to "compose" a
    camera instead of giving you preset options. This also allows you to
    define behaviour such as zooming to what best fits your needs.

--- src/main.rs
+++ src/main.rs
@@ -1,57 +1,72 @@
-use kiss3d::prelude::*;
+use three_d::{
+    Camera, ClearState, ColorMaterial, CpuMesh, FrameOutput, Geometry, Gm, Indices, Mesh,
+    Positions, Quat, Rotation3, Srgba, Window, WindowSettings, degrees, radians, vec3,
+};
 
-// Ideally, these variables should by "dynamic".
-// This is something to take note of in the future.
-//
-// SPACING should be greater than LINE_WIDTH.
-
-/// Grid is square and has edges:
-/// (r, r), (-r, r), (r, -r), (-r, -r)
-const RADIUS: f32 = 250.0;
-
-/// Spacing between grid lines.
-const SPACING: f32 = 25.0;
-
-/// Width of grid lines in pixels.
+/// Width of grid lines in units.
 const LINE_WIDTH: f32 = 1.0;
 
-#[kiss3d::main]
-async fn main() {
-    let mut window = Window::new("2D Grid").await;
-    let mut camera = OrbitCamera3d::default();
-    let mut scene = SceneNode3d::empty();
+/// Rotates a triangle.
+///
+/// # Panics
+///
+/// Window failed to be created.
+pub fn main() {
+    let window = Window::new(WindowSettings {
+        title: "Triangle!".to_string(),
+        max_size: Some((1280, 720)),
+        ..Default::default()
+    })
+    .unwrap();
 
-    camera.set_dist(RADIUS * 3.0);
+    let ctx = window.gl();
 
-    // Draws a 2D grid.
-    //
-    // In the future, this should be a 3D grid. A toggle for a
-    // "2D mode" would lock the camera and hide z grid lines.
-    while window.render_3d(&mut scene, &mut camera).await {
-        eprintln!("{}", camera.dist());
-        let mut i = -RADIUS;
+    // A "perspective" camera (as opposed to an orthographic one) is
+    // often preferred as it's more natural to the eye. Though, for a
+    // graphing tool, orthographic may be desired for its consistency.
+    let mut camera = Camera::new_perspective(
+        window.viewport(),
+        vec3(0.0, 0.0, 5.0),
+        vec3(0.0, 0.0, 0.0),
+        vec3(0.0, 1.0, 0.0),
+        degrees(45.0),
+        0.1,
+        10.0,
+    );
 
-        // Draws the grid lines.
-        while i <= RADIUS {
-            // Horizontal.
-            window.draw_line(
-                Vec3::new(-RADIUS, i, 0.0),
-                Vec3::new(RADIUS, i, 0.0),
-                WHITE,
-                LINE_WIDTH,
-                false,
-            );
+    let positions = Positions::F32(vec![
+        vec3(-5.0, -LINE_WIDTH / 2.0, 0.0),
+        vec3(-5.0, LINE_WIDTH / 2.0, 0.0),
+        vec3(5.0, -LINE_WIDTH / 2.0, 0.0),
+        vec3(5.0, LINE_WIDTH / 2.0, 0.0),
+    ]);
 
-            // Vertical.
-            window.draw_line(
-                Vec3::new(i, -RADIUS, 0.0),
-                Vec3::new(i, RADIUS, 0.0),
-                WHITE,
-                LINE_WIDTH,
-                false,
-            );
+    let indices = Indices::U8(vec![0, 1, 2, 1, 2, 3]);
 
-            i += SPACING;
-        }
-    }
+    // redundant field names lint doesn't trigger
+    let cpu_mesh = CpuMesh {
+        positions,
+        indices,
+        ..Default::default()
+    };
+
+    let mut model = Gm::new(Mesh::new(&ctx, &cpu_mesh), ColorMaterial::default());
+    model.set_animation(|time| Quat::from_angle_y(radians(time * 0.0005)).into());
+
+    window.render_loop(move |frame_input| {
+        camera.set_viewport(frame_input.viewport);
+
+        #[expect(
+            clippy::as_conversions,
+            reason = "f32 works for anything under ~1e28 years"
+        )]
+        model.animate(frame_input.accumulated_time as f32);
+
+        frame_input
+            .screen()
+            .clear(ClearState::color_and_depth(0.8, 0.8, 0.8, 1.0, 1.0))
+            .render(&camera, &model, &[]);
+
+        FrameOutput::default()
+    });
 }
